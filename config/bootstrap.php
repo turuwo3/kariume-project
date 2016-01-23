13f59ce62e1fb73c77e9d51e8c3c559bf2f0245f
root <root@localhost.localdomain>
2016-01-23 22:49:21 +0900

    bootstrapを修正

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,10 +4,13 @@ use TRW\Core\Configure;
 use TRW\Router\Router;
 use TRW\ActiveRecord\BaseRecord;
 use TRW\ActiveRecord\Database\Driver\MySql;
+use TRW\Exception\ErrorHandler;
 
 //ini_set('display_errors',1);
 
 Configure::load(dirname(__FILE__) . '/config.php');
+
+set_exception_handler('TRW\Exception\ErrorHandler::handleException');
 
 BaseRecord::setConnection(new MySql(Configure::read('Database', 'MySql')));
 
